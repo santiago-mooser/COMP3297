1c02cb443e0bc7899fc0d3716d7bec10d55b73d9
Santiago <santiago.mooser@pm.me>
2021-02-03 09:52:05 +0800

    feat(docker,django): Added ability to programmatically add a superuser

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,5 +2,6 @@
 
 echo "Sleeping to allow database to initialize"
 sleep 3.5
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${django_admin}', '${django_email}', '${django_password}')" | python manage.py shell
 python3 manage.py migrate
 python3 manage.py runserver 0:${django_port}

9f2b7033127702f2cb3c624ecf338cb46c78e79c
Santiago <santiago.mooser@pm.me>
2021-02-03 10:28:33 +0800

    misc(django): renamed django project from `app` to `django_project`

--- app/django_project/django_project/urls.py
+++ app/django_project/django_project/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/

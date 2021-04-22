2f3c96bf4cb969c8b2fb883cf55f4401d1c2c16b
pkong-ds <pkong.work@gmail.com>
2021-04-22 17:14:39 +0800

    build: initialise new case page

--- sse_finder/views.py
+++ sse_finder/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .forms import Homepage
+from .forms import Homepage, New_case, New_location
+from .models import *
 
 # Create your views here.
 
@@ -21,7 +22,55 @@ def add_location(request):
     return
 
 def add_case(request):
-    return
+
+    template = loader.get_template('pages/new_case.html')
+
+    if request.method == 'POST':
+        form = New_case(request.POST)
+
+        # check if the form is valid
+        if form.is_valid():
+
+            # Extract data from form
+            name     = form.cleaned_data['case_name']
+            num_case = form.cleaned_data['case_number']
+            pid      = form.cleaned_data['personal_id']
+            dob      = form.cleaned_data['date_of_birth']
+            doo      = form.cleaned_data['date_of_onset']
+            dot      = form.cleaned_data['date_of_test']
+
+            # Create new instance of model Case
+            new_case = Case(
+                case_name=name,
+                case_number=num_case,
+                personal_id=pid,
+                date_of_birth=dob,
+                date_of_onset=doo,
+                date_of_test=dot,
+                # TODO: add class variable Event in forms.py
+                Event=Location(),
+            )
+            try:
+                new_case.save()
+            except:
+                messages.error(request, "Internal server error! Please reload page.")
+                # TODO: redirect to homepage
+                return HttpResponseRedirect('/add/case')
+            
+            messages.success(request, "Details successfully saved.")
+            # TODO: redirect to case details
+            return HttpResponseRedirect('/add/case')
+
+        # If form is invalid
+        else:
+            messages.error(request, "Please enter valid details.")
+            return HttpResponseRedirect('/add/case')
+    # If method is not POST
+    else:
+        form = New_case() # empty form instance
+
+    context = {'form':form}
+    return HttpResponse(template.render(context, request))
 
 def location_details(request, loc_name):
     return

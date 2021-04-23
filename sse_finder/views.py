5f12d904f6286fb7496146621b92041c40c36306
Santiago <santiago.mooser@pm.me>
2021-04-23 08:52:29 +0800

    feat(views.py): Added location_details view

--- sse_finder/views.py
+++ sse_finder/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.http.response import HttpRespons, HttpResponse
+from django.template import loader
+from .models import *
+from django.contrib import messages
 # Create your views here.
 
 
@@ -14,7 +16,25 @@ def add_case(request):
     return
 
 def location_details(request, loc_name):
-    return
+    template = loader.get_template('pages/location_details.html')
+    context = {}
+
+    try:
+        location = Location.objects.get(name=loc_name)
+    except:
+        messages.error(request, "Location not found!")
+        return HttpResponse(template.render(context, request))
+
+
+    context.update({
+        "name": location.name,
+        "location": location.location,
+        "address": location.address,
+        "date_of_event": location.date_of_event,
+        "description_of_event": location.description_of_event,
+    })
+
+    return HttpResponse(template.render(context, request))
 
 def case_details(request, loc_name):
     return

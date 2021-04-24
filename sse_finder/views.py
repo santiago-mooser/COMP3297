a17744fad26a54ad0e9b32eebfb6124f29c3bc1e
Tridib Mitra <mitra.tridib13@gmail.com>
2021-04-24 13:14:49 +0530

    setup location links on home page and setup location deets page

--- sse_finder/views.py
+++ sse_finder/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpRespons, HttpResponse
+from django.http.response import HttpResponse
 from django.template import loader
 from django.contrib import messages
 from django.shortcuts import render
@@ -41,8 +41,11 @@ def add_case(request):
     return
 
 def location_details(request, loc_name):
+
     template = loader.get_template('pages/location_details.html')
     context = {}
+
+    return HttpResponse(template.render(context, request))
 
 def case_details(request, loc_name):
     return

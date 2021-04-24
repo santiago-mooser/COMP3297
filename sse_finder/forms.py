bcf635b0ac68346638e29c606065f26205a7c6c0
pkong-ds <pkong.work@gmail.com>
2021-04-24 19:54:03 +0800

    feat: date picker on new case/location

--- sse_finder/forms.py
+++ sse_finder/forms.py
@@ -22,9 +22,9 @@ class New_location(forms.Form):
     address                 = forms.CharField(max_length=250, label='Address'
                                                 , widget= forms.TextInput(attrs={'placeholder':'Don\'t worry, backend will handle this'}))
     date_of_event           = forms.DateField(label='Date of Event'
-                                                , widget= forms.TextInput(attrs={'placeholder':'YYYY-mm-dd'}))
+                                                , widget=DateInput)
     description_of_event    = forms.CharField(max_length=1000, label='Description of event'
-                                                , widget= forms.TextInput(attrs={'placeholder':'Briefly describe event'}))
+                                                , widget= forms.Textarea(attrs={'placeholder':'Briefly describe event', 'rows':4, 'cols': 40}))
     
 
     class Meta:
@@ -48,11 +48,11 @@ class New_case(forms.Form):
     personal_id     = forms.CharField(max_length=25, label='ID'
                                          , widget= forms.TextInput(attrs={'placeholder':'Enter case personal id'}))
     date_of_birth   = forms.DateField(label='Date of Birth'
-                                         , widget= forms.TextInput(attrs={'placeholder':'YYYY-mm-dd'}))
+                                         , widget=DateInput)
     date_of_onset   = forms.DateField(label='Date of Symptoms Onset'
-                                         , widget= forms.TextInput(attrs={'placeholder':'YYYY-mm-dd'}))
+                                         , widget=DateInput)
     date_of_test    = forms.DateField(label='Date of Positive Test'
-                                         , widget= forms.TextInput(attrs={'placeholder':'YYYY-mm-dd'}))
+                                         , widget=DateInput)
     case_event      = forms.ModelChoiceField(label='Event'
                                          , queryset=Location.objects.all().order_by('name')
                                          , required=False)
@@ -60,6 +60,7 @@ class New_case(forms.Form):
     class Meta:
         model   = Case
         fields  = ('case_name', 'case_number', 'personal_id', 'date_of_birth', 'date_of_onset', 'date_of_test', 'case_event')
+        
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)

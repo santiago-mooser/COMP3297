e4310a1d3bfbb0aac696972465d9f8e531334fdb
WW-prog <78344414+WW-prog@users.noreply.github.com>
2021-05-04 21:58:51 +0800

    Add files via upload

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,22 +81,8 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': env.dj_db_url('DATABASE_URL')
-# }
-
-# local dev db config
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '3297-group',
-        'USER': 'postgres',
-        'PASSWORD': 'Buddy12teddy',
-        'HOST': 'localhost',
-        'PORT': 5433,
-    }
-
+    'default': env.dj_db_url('DATABASE_URL')
 }
 
 # local dev db config
@@ -151,6 +137,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))

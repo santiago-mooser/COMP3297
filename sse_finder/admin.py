f8e42e1345b1be63bd830efffcb209b20e828cf6
Iestyn Lam <48487929+iestynlam@users.noreply.github.com>
2021-05-05 22:49:20 +0800

    Adding CHP_User to admin

--- sse_finder/admin.py
+++ sse_finder/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Case, Location
+from .models import Case, Location, CHP_User
 
 # Register your models here.
 admin.site.register(Case)
 admin.site.register(Location)
+admin.site.register(CHP_User)
